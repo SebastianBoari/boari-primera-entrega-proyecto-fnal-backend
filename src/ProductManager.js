2ee2af4577a966b2a456e4ed472249d7cc14f44e
Sebastian Boari <luciosebastianboari@gmail.com>
2023-04-22 02:03:10 -0300

    feat: post method, async/await fixed

---
+++ src/ProductManager.js
@@ -0,0 +1,121 @@
+import fs from 'fs';
+
+export default class ProductManager {
+
+    #products 
+    path
+
+    constructor (path){
+        this.#products = []
+        this.path = path
+        this.#loadProducts()
+    };
+
+    async #loadProducts(){
+        try {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            this.#products = JSON.parse(data);
+        } catch (err) {
+            console.error(`Error al leer el archivo: ${err}`);
+            this.#products = [];
+        };
+    };
+    async #saveProducts() {
+        try {
+            const data = JSON.stringify(this.#products, null, 2); 
+            await fs.promises.writeFile(this.path, data);
+        } catch (err) {
+            console.error(`Error al guardar el archivo: ${err}`);
+        };
+    };
+
+    getProducts () {
+        return this.#products;
+    };
+
+    getProductById (id) {
+        return this.#products.find((product) => product.id === id);
+    };
+
+    #areFieldComplete (title, description, price, status, code, stock) {
+        if (!title || !description || !price || (typeof status !== "boolean") || !code || !stock) {
+            console.error("Por favor completa todos los campos");
+            return false; 
+        } else { 
+            return true; 
+        };
+    };
+
+    #isNotDuplicate (code) {
+        if(this.#products.find((product) => product.code === code) !== undefined){
+            console.error("El codigo ya existe, posible producto duplicado.");
+            return false;
+        } else {
+            return true;
+        };
+    };
+
+    #idGenerator () {
+        let id = 0;
+        if(this.#products.length === 0){
+            id = 1;
+        } else {
+            id = this.#products[this.#products.length-1].id + 1;
+        };
+        return id;
+    };
+
+    addProduct (title, description, price, status, thumbnail, code, stock) {
+        if(this.#areFieldComplete(title, description, price, status, code, stock) && this.#isNotDuplicate(code)){
+            const newProduct = {
+                id: this.#idGenerator(),
+                title: title,
+                description: description,
+                price: price,
+                status: true,
+                thumbnail: thumbnail || [],
+                code: code,
+                stock: stock
+            };
+            this.#products.push(newProduct);
+            this.#saveProducts();
+            console.log("El producto se ha creado satisfactoriamente.");
+        };
+    };
+
+    updateProduct (id, updatedProduct) {
+        const index = this.#products.findIndex((product) => product.id === id);
+        if (index !== -1) {
+            this.#products[index] = {...this.#products[index], ...updatedProduct};
+            this.#saveProducts();
+        } else {
+            console.error(`No se encontró el producto con id ${id}`);
+        };
+    };
+
+    deleteProduct (id) {
+        const index = this.#products.findIndex((product) => product.id === id);
+        if (index !== -1) {
+            this.#products.splice(index, 1);
+            this.#saveProducts();
+        } else {
+            console.error(`No se encontró el producto con id ${id}`);
+        };
+    };
+};
+
+export const productManager = new ProductManager('./products.json');
+
+/*
+productManager.addProduct("producto prueba 1", "Este es un producto prueba 1", 100, true, "", "abc121", 21);
+productManager.addProduct("producto prueba 2", "Este es un producto prueba 2", 200, true,"", "abc122", 22);
+productManager.addProduct("producto prueba 3", "Este es un producto prueba 3", 300, false, "", "abc123", 23);
+productManager.addProduct("producto prueba 4", "Este es un producto prueba 4", 400, true, "", "abc124", 24);
+productManager.addProduct("producto prueba 5", "Este es un producto prueba 5", 500, true, "", "abc125", 25);
+productManager.addProduct("producto prueba 6", "Este es un producto prueba 6", 600, true, "", "abc126", 26);
+productManager.addProduct("producto prueba 7", "Este es un producto prueba 7", 700, true, "", "abc127", 27);
+productManager.addProduct("producto prueba 8", "Este es un producto prueba 8", 800, true, "", "abc128", 28);
+productManager.addProduct("producto prueba 9", "Este es un producto prueba 9", 900, true, "", "abc129", 29);
+productManager.addProduct("producto prueba 10", "Este es un producto prueba 10", 1000, true, "Sin imagen", "abc130", 30);
+productManager.addProduct("PRODUCTO", "PRUEBA 11", 9999, true, ['url1', 'url2', 'url3'], "z1z2z3", 30110); 
+*/
--- src/server.js
+++ src/server.js
@@ -1,11 +1,43 @@
-const express = require('express');
+import express from 'express';
+import { productManager } from './ProductManager.js';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send("Init")
+// Devolver todos los productos o con un limite
+// Ruta de prueba: http://localhost:8080/products?limit=1
+app.get('/products', (req, res) => {
+    const limit = Number(req.query.limit);
+    limit? res.status(200).send(productManager.getProducts().slice(0, limit)) : res.status(200).send(productManager.getProducts()); 
+});
+
+// Devolver un producto por su id
+// Ruta de prueba: http://localhost:8080/products/6
+app.get('/products/:id', (req, res) => {
+    const id = Number(req.params.id);
+    res.status(200).send(productManager.getProductById(id));
+});
+
+// Crear un producto
+// Producto de prueba: http://localhost:8080/products/
+/*
+{
+    "title": "PRUEBA PRUEBA",
+    "description": "PRUEBA 12",
+    "price": 7272727272,
+    "status": true,
+    "thumbnail": ["url1", "urlasdlasd", "aksdaklsd"],
+    "code": "123123123123",
+    "stock": 900
+}
+*/
+app.post('/products', (req, res) => {
+    const body = req.body;
+
+    res.status(200).send(productManager.addProduct(body.title, body.description, body.price, body.status, body.thumbnail, body.code, body.stock));
 });
 
 
-
-app.listen(8080, ()=>console.log('Server Up'));
+app.listen(8080, () => {
+    console.log('Server Up on port 8080');
+});
